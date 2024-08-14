c8329a8f70fefb4fcce61b7111a3cac095aaa67b
Emery Chikezie Ogechukwu <emerychikezie2@gmail.com>
2024-08-14 15:20:37 +0100

    updated deployment
    deployed app on a new firebase hosting setup

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -4,12 +4,12 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQcylB4n4_6696-pHyWXAJQmiGvxw56m0",
-  authDomain: "scissor-8c3bf.firebaseapp.com",
-  projectId: "scissor-8c3bf",
-  storageBucket: "scissor-8c3bf.appspot.com",
-  messagingSenderId: "249324222161",
-  appId: "1:249324222161:web:3da3606addd5425a4ff7f0",
+  apiKey: "AIzaSyCEeWo1os-n_SmcVXjVEvQ2baAaAzW2vTw",
+  authDomain: "scissor-url-47c6d.firebaseapp.com",
+  projectId: "scissor-url-47c6d",
+  storageBucket: "scissor-url-47c6d.appspot.com",
+  messagingSenderId: "976710948717",
+  appId: "1:976710948717:web:38adfb35372574b516ac64",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,7 +18,7 @@ const firestore = getFirestore(app);
 
 if (process.env.NODE_ENV === "development") {
   connectFirestoreEmulator(firestore, "localhost", 8080);
-  connectAuthEmulator(auth, "http://localhost:9099");
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
 }
 
 export { app, firestore, auth };
